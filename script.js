4eb290a913db81a385da8467a3e9c42c06dff68f
VivekanandanDevendiran <dvivekanandan3@gmail.com>
2024-09-05 08:49:39 +0530

    1st commit

---
+++ script.js
@@ -0,0 +1,26 @@
+// Get the display element
+const display = document.getElementById("display");
+
+function appendNumber(number) {
+    display.value += number;
+}
+
+function appendOperator(operator) {
+    display.value += operator;
+}
+
+function clearDisplay() {
+    display.value = "";
+}
+
+function deleteDigit() {
+    display.value = display.value.slice(0, -1);
+}
+
+function calculate() {
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = "Error";
+    }
+}
